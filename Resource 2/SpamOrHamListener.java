0e307d10060b3d819e2337829dd7576d32013bfd
DougCodez <98244802+DougCodez@users.noreply.github.com>
2023-04-06 22:28:46 -0400

    Update SpamOrHamListener.java

--- Resource 2/SpamOrHamListener.java
+++ Resource 2/SpamOrHamListener.java
@@ -13,8 +13,6 @@ public class SpamOrHamListener implements Listener {
 
     private final RNNModel rnnModel;
 
-
-
     public SpamOrHamListener(RNNModel rnnModel) {
         this.rnnModel = rnnModel;
     }
@@ -24,13 +22,20 @@ public class SpamOrHamListener implements Listener {
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         String message = event.getMessage();
+        //Remember make the sure model was previously trained so that way we can determine a prediction
         if (PredictionUtils.containsSpam(rnnModel, message)) {
             event.setCancelled(true);
             player.sendMessage("You can't say that!");
+
+            //We use the decimal formatter to round the prediction to 3 decimal places
             DecimalFormat df = new DecimalFormat("#.###");
             df.setRoundingMode(RoundingMode.FLOOR);
             double prediction = Double.parseDouble(df.format(PredictionUtils.getPrediction()));
+
+            //We then store the message and the prediction in the prediction map
             PredictionUtils.getPredictionMap().putIfAbsent(message, prediction);
+
+            //We then alert the opped player that the message was spam and that's if the prediction was greater than 0.85 (threshold)
             if (player.isOp()) {
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&c&lSFilter: &7{&fCaptured Phrase &8| &fPrediction&7}&f: " + "&7{&f" + message + " &8| &f" +  prediction + "&7}"));
             }
